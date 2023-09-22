f7bf35f1d3136bc011de9697e7501bac71aaad17
Dhruv Rana <dhruvrana4@gmail.com>
2023-09-22 22:49:30 +0530

    feat: Splash screen complete

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter Template ",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SpinKitCircle(
+              color: Colors.white,
+              size: 50.0,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
